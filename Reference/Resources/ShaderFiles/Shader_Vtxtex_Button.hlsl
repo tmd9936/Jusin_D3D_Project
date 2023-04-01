ab8878a565b5b4bbfc4848d7b453017428fef022
Seung Hwan Baek <ghks9936@naver.com>
2023-04-01 21:41:11 +0900

    월드맵 버튼 변경

--- Reference/Resources/ShaderFiles/Shader_Vtxtex_Button.hlsl
+++ Reference/Resources/ShaderFiles/Shader_Vtxtex_Button.hlsl
@@ -75,13 +75,9 @@ PS_OUT PS_MAIN(PS_IN In)
 {
 	PS_OUT		Out = (PS_OUT)0;
 
-	Out.vColor = g_Texture.Sample(LinearSampler, In.vTexUV);
+	Out.vColor = g_Texture.Sample(PointSampler, In.vTexUV);
 
 	if (Out.vColor.a < 0.1)
-		discard;
-
-	float2 coords = In.vTexUV * 7.f;
-	if (ShouldDiscard(coords, 7.f, 1.f))
 		discard;
 
 	return Out;
@@ -92,16 +88,32 @@ PS_OUT PS_MAIN_COLOR(PS_IN In)
 {
 	PS_OUT		Out = (PS_OUT)0;
 
-	Out.vColor = g_Texture.Sample(LinearSampler, In.vTexUV);
+	Out.vColor = g_Texture.Sample(PointSampler, In.vTexUV);
 
 	if (Out.vColor.a < 0.1)
 		discard;
 
-	float2 coords = In.vTexUV * 7.f;
-	if (ShouldDiscard(coords, 7.f, 1.f))
+	float2 coords = In.vTexUV * 5.f;
+	if (ShouldDiscard(coords, 5.f, 1.f))
 		discard;
 
 	Out.vColor = g_vColor;
+
+	return Out;
+}
+
+PS_OUT PS_MAIN_ROUND(PS_IN In)
+{
+	PS_OUT		Out = (PS_OUT)0;
+
+	Out.vColor = g_Texture.Sample(PointSampler, In.vTexUV);
+
+	if (Out.vColor.a < 0.1)
+		discard;
+
+	float2 coords = In.vTexUV * 5.f;
+	if (ShouldDiscard(coords, 5.f, 1.f))
+		discard;
 
 	return Out;
 }
@@ -132,5 +144,18 @@ technique11		DefaultTechnique
 		HullShader = NULL;
 		DomainShader = NULL;
 		PixelShader = compile ps_5_0 PS_MAIN_COLOR();
+	}
+
+	pass Button_Idle_Round
+	{
+		SetRasterizerState(RS_Default);
+		SetDepthStencilState(DSS_Enable_ZTest_Disable_ZWrite, 0);
+		SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+		VertexShader = compile vs_5_0 VS_MAIN();
+		GeometryShader = NULL;
+		HullShader = NULL;
+		DomainShader = NULL;
+		PixelShader = compile ps_5_0 PS_MAIN_ROUND();
 	}
 }
