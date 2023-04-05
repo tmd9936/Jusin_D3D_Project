21498bc0b33168010da567e5297302d3da95051e
Seung Hwan Baek <ghks9936@naver.com>
2023-04-05 19:13:52 +0900

    우너거리 몬스터 거리 유지 속도 변경

--- Tool/Private/StageEnemyMonster.cpp
+++ Tool/Private/StageEnemyMonster.cpp
@@ -233,7 +233,7 @@ _uint CStageEnemyMonster::State_Tick(const _double& TimeDelta)
 					else
 					{
 						if (m_pTransformCom->Go_BackWard_Look_Pos(pTargetTransform->Get_State(CTransform::STATE_POSITION), m_pTransformCom->Get_State(CTransform::STATE_POSITION)
-							+ m_pTransformCom->Get_State(CTransform::STATE_LOOK) * -2.f, _float(TimeDelta), 0.5f, m_pNavigationCom))
+							+ m_pTransformCom->Get_State(CTransform::STATE_LOOK) * -2.f, _float(TimeDelta * 1.5), 0.5f, m_pNavigationCom))
 						{}
 					}
 				}
