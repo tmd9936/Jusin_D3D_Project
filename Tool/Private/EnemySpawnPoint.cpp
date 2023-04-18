9db83d27b5f1d3b427232bbbb7b6953e4ac3d865
Seung Hwan Baek <ghks9936@naver.com>
2023-04-18 11:46:09 +0900

    적 스폰 포인트 클래스 생성

---
+++ Tool/Private/EnemySpawnPoint.cpp
@@ -0,0 +1,216 @@
+#include "stdafx.h"
+#include "EnemySpawnPoint.h"
+
+#include "GameInstance.h"
+
+CEnemySpawnPoint::CEnemySpawnPoint(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CGameObject(pDevice, pContext)
+{
+
+}
+
+CEnemySpawnPoint::CEnemySpawnPoint(const CEnemySpawnPoint& rhs)
+	: CGameObject(rhs)
+{
+
+}
+
+HRESULT CEnemySpawnPoint::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CEnemySpawnPoint::Initialize(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg)
+{
+	if (nullptr != pArg)
+	{
+		m_Desc.m_position = (*(ENEMY_SPAWN_POINT_DESC*)(pArg)).m_position;
+		m_Desc.m_type = (*(ENEMY_SPAWN_POINT_DESC*)(pArg)).m_type;
+		m_Desc.m_spawnRadius = (*(ENEMY_SPAWN_POINT_DESC*)(pArg)).m_spawnRadius;
+	}
+
+	if (FAILED(__super::Initialize(pLayerTag, iLevelIndex, pArg)))
+		return E_FAIL;
+
+	if (FAILED(Add_Components()))
+		return E_FAIL;
+
+	m_pTransformCom->Set_Pos(m_Desc.m_position.x, m_Desc.m_position.y, m_Desc.m_position.z);
+
+	m_eRenderId = RENDER_NONBLEND;
+
+	return S_OK;
+}
+
+HRESULT CEnemySpawnPoint::Initialize(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath)
+{
+	if (FAILED(__super::Initialize(pLayerTag, iLevelIndex, filePath)))
+		return E_FAIL;
+
+	if (filePath)
+	{
+		Load_By_JsonFile(filePath);
+		m_strSaveJsonPath = filePath;
+	}
+
+	if (FAILED(Add_Components()))
+		return E_FAIL;
+
+	m_pTransformCom->Set_Pos(m_Desc.m_position.x, m_Desc.m_position.y, m_Desc.m_position.z);
+
+	m_eRenderId = RENDER_NONLIGHT;
+
+	return S_OK;
+}
+
+_uint CEnemySpawnPoint::Tick(_double TimeDelta)
+{
+	m_pAABB->Tick(m_pTransformCom->Get_WorldMatrix_Matrix());
+	return _uint();
+}
+
+_uint CEnemySpawnPoint::LateTick(_double TimeDelta)
+{
+	m_pRendererCom->Add_RenderGroup(m_eRenderId, this);
+
+	return _uint();
+}
+
+HRESULT CEnemySpawnPoint::Render()
+{
+	if (FAILED(SetUp_ShaderResources()))
+		return E_FAIL;
+
+#ifdef _DEBUG
+	m_pAABB->Render();
+#endif // _DEBUG
+
+	return S_OK;
+}
+
+_bool CEnemySpawnPoint::Save_By_JsonFile_Impl(Document& doc, Document::AllocatorType& allocator)
+{
+	Value EnemySpawnPointDesc(kObjectType);
+	{
+		Value m_position(kObjectType);
+		{
+			m_position.AddMember("x", m_Desc.m_position.x, allocator);
+			m_position.AddMember("y", m_Desc.m_position.y, allocator);
+			m_position.AddMember("z", m_Desc.m_position.z, allocator);
+		}
+		EnemySpawnPointDesc.AddMember("m_position", m_position, allocator);
+
+		EnemySpawnPointDesc.AddMember("m_type", m_Desc.m_type, allocator);
+		EnemySpawnPointDesc.AddMember("m_spawnRadius", m_Desc.m_spawnRadius, allocator);
+	}
+	doc.AddMember("EnemySpawnPointDesc", EnemySpawnPointDesc, allocator);
+
+	return true;
+}
+
+_bool CEnemySpawnPoint::Load_By_JsonFile_Impl(Document& doc)
+{
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
+
+	CGameInstance* pGameInstance = CGameInstance::GetInstance();
+
+	const Value& EnemySpawnPointDesc = doc["EnemySpawnPointDesc"];
+
+	const Value& m_position = EnemySpawnPointDesc["m_position"];
+	m_Desc.m_position = _float3(m_position["x"].GetFloat(), m_position["y"].GetFloat(), m_position["z"].GetFloat());
+
+	m_Desc.m_type = EnemySpawnPointDesc["m_type"].GetUint();
+	m_Desc.m_spawnRadius = EnemySpawnPointDesc["m_spawnRadius"].GetFloat();
+
+	return true;
+}
+
+
+HRESULT CEnemySpawnPoint::Add_Components()
+{
+	CGameInstance* pGameInstance = CGameInstance::GetInstance();
+
+	/* For.Com_PickingCube */
+	if (FAILED(pGameInstance->Add_Component(CPickingCube::familyId, this, LEVEL_STATIC, TEXT("Prototype_Component_PickingCube"),
+		(CComponent**)&m_pPickingCube, nullptr)))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	CTransform::TRANSFORMDESC		TransformDesc = { 10.f, XMConvertToRadians(90.0f) };
+	if (FAILED(pGameInstance->Add_Component(CTransform::familyId, this, LEVEL_STATIC, TEXT("Prototype_Component_Transform"),
+		(CComponent**)&m_pTransformCom, &TransformDesc)))
+		return E_FAIL;
+
+	/* For.Com_Renderer */
+	if (FAILED(pGameInstance->Add_Component(CRenderer::familyId, this, LEVEL_STATIC, TEXT("Prototype_Component_Renderer"),
+		(CComponent**)&m_pRendererCom, nullptr)))
+		return E_FAIL;
+
+	/* For.Com_AABB*/
+	CCollider::COLLIDER_DESC		ColliderDesc;
+	ZeroMemory(&ColliderDesc, sizeof ColliderDesc);
+	ColliderDesc.vScale = _float3(1.f, 2.f, 1.f);
+	ColliderDesc.vPosition = _float3(0.0f, ColliderDesc.vScale.y * 0.5f, 0.f);
+	if (FAILED(pGameInstance->Add_Component(CCollider::familyId, this, LEVEL_STATIC, TEXT("Prototype_Component_Collider_AABB"),
+		(CComponent**)&m_pAABB, &ColliderDesc)))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+
+HRESULT CEnemySpawnPoint::SetUp_ShaderResources()
+{
+	return S_OK;
+}
+
+CEnemySpawnPoint* CEnemySpawnPoint::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CEnemySpawnPoint* pInstance = new CEnemySpawnPoint(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Created CEnemySpawnPoint");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject* CEnemySpawnPoint::Clone(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg)
+{
+	CEnemySpawnPoint* pInstance = new CEnemySpawnPoint(*this);
+
+	if (FAILED(pInstance->Initialize(pLayerTag, iLevelIndex, pArg)))
+	{
+		MSG_BOX("Failed to Cloned CEnemySpawnPoint");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject* CEnemySpawnPoint::Clone(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath)
+{
+	CEnemySpawnPoint* pInstance = new CEnemySpawnPoint(*this);
+
+	if (FAILED(pInstance->Initialize(pLayerTag, iLevelIndex, filePath)))
+	{
+		MSG_BOX("Failed to Cloned CEnemySpawnPoint");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+
+void CEnemySpawnPoint::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pTransformCom);
+	Safe_Release(m_pRendererCom);
+	Safe_Release(m_pPickingCube);
+	Safe_Release(m_pAABB);
+}
---
+++ Tool/Public/EnemySpawnPoint.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CTransform;
+class CRenderer;
+class CPickingCube;
+class CAABB;
+END
+
+BEGIN(Client)
+
+class CEnemySpawnPoint final : public CGameObject
+{
+public:
+	typedef struct Enemy_Spawn_Point_Desc
+	{
+		_float3		m_position;
+		_uint		m_type;
+		_float		m_spawnRadius;
+
+	} ENEMY_SPAWN_POINT_DESC;
+
+public:
+	enum  TYPE
+	{
+		TYPE_PRE_CREATE,
+		TYPE_NORMAL_APPEARANCE,
+		TYPE_SURPIRSE_APPEARANCE,
+		TYPE_BOSS_APPEARANCE,
+		TYPE_END
+	};
+
+private:
+	CEnemySpawnPoint(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CEnemySpawnPoint(const CEnemySpawnPoint& rhs);
+	virtual ~CEnemySpawnPoint() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg) override;
+	virtual HRESULT Initialize(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath);
+
+
+	virtual _uint Tick(_double TimeDelta) override;
+	virtual _uint LateTick(_double TimeDelta) override;
+	virtual HRESULT Render() override;
+
+protected:
+	virtual _bool			Save_By_JsonFile_Impl(Document& doc, Document::AllocatorType& allocator);
+	virtual _bool			Load_By_JsonFile_Impl(Document& doc);
+
+private:
+	HRESULT Add_Components();
+	HRESULT SetUp_ShaderResources(); 
+
+private:
+	CTransform*					m_pTransformCom = { nullptr };
+	CRenderer*					m_pRendererCom = { nullptr };
+	CPickingCube*				m_pPickingCube = { nullptr };
+	CAABB*						m_pAABB = { nullptr };
+
+private:
+	ENEMY_SPAWN_POINT_DESC		m_Desc = { };
+	//TYPE						m_eType = { TYPE_END };
+
+public:
+	static CEnemySpawnPoint* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg = nullptr) override;
+	virtual CGameObject* Clone(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath = nullptr);
+	virtual void Free() override;
+};
+
+END
