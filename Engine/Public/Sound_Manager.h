13856a9d232708e9eccf0e9e70b7d34d97964a5f
Seung Hwan Baek <ghks9936@naver.com>
2023-05-08 12:41:19 +0900

    사운드 초기화 구문 추가

--- Engine/Public/Sound_Manager.h
+++ Engine/Public/Sound_Manager.h
@@ -67,7 +67,7 @@ private:
 	unordered_map<_tchar*, FMOD_SOUND*> m_mapSound;
 
 	// FMOD_CHANNEL : 재생하고 있는 사운드를 관리할 객체 
-	FMOD_CHANNEL* m_pChannelArr[MAXCHANNEL];
+	FMOD_CHANNEL* m_pChannelArr[MAXCHANNEL] = { nullptr };
 
 	// 사운드 ,채널 객체 및 장치를 관리하는 객체 
 	FMOD_SYSTEM* m_pSystem = { nullptr };
