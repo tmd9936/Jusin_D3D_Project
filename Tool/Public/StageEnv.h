9ec65994aac58caab60cc9dd92934c0c3f8146ca
Seung Hwan Baek <ghks9936@naver.com>
2023-05-10 10:07:48 +0900

    StageEnv 추가

---
+++ Tool/Public/StageEnv.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CTransform;
+class CRenderer;
+class CTexture;
+class CShader;
+class CModel;
+END
+
+BEGIN(Client)
+
+class CStageEnv final : public CGameObject
+{
+public:
+	typedef struct Anim_Env_Desc
+	{
+		_float3		vPos;
+		_float3		vScale;
+		_float3		vRotate;
+
+		_uint		animIndex;
+		wstring		ModelPrototypeTag;
+	} ANIM_ENV_DESC;
+
+private:
+	explicit CStageEnv(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	explicit CStageEnv(const CStageEnv& rhs);
+	virtual ~CStageEnv() = default;
+
+public:
+	virtual HRESULT			Initialize_Prototype() override;
+	virtual HRESULT			Initialize(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg) override;
+	virtual HRESULT			Initialize(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath);
+
+	virtual _uint			Tick(_double TimeDelta) override;
+	virtual _uint			LateTick(_double TimeDelta) override;
+	virtual HRESULT			Render() override;
+	virtual HRESULT			Render_Laplacian() override;
+
+protected:
+	virtual _bool			Save_By_JsonFile_Impl(Document& doc, Document::AllocatorType& allocator);
+	virtual _bool			Load_By_JsonFile_Impl(Document& doc);
+
+
+private:
+	HRESULT					Add_Components();
+	HRESULT					Add_Components_By_File();
+
+	HRESULT					SetUp_ShaderResources();
+
+private:
+	CTransform* m_pTransformCom = { nullptr };
+	CRenderer* m_pRendererCom = { nullptr };
+	CShader* m_pShaderCom = { nullptr };
+	CModel* m_pModelCom = { nullptr };
+
+private:
+	ANIM_ENV_DESC			m_AnimEnvDesc = { };
+
+public:
+	static CStageEnv* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(const _tchar* pLayerTag, _uint iLevelIndex, void* pArg = nullptr) override;
+	virtual CGameObject* Clone(const _tchar* pLayerTag, _uint iLevelIndex, const char* filePath = nullptr);
+	virtual void Free() override;
+};
+
+END
